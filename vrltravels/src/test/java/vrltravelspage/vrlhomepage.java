3ff7cceed81e3f674709f1d8f79bc23b39774ed6
AthiraPappachan <athirapappachan1992@gmail.com>
2021-12-01 17:40:37 +0000

    New project

---
+++ vrltravels/src/test/java/vrltravelspage/vrlhomepage.java
@@ -0,0 +1,31 @@
+package vrltravelspage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class vrlhomepage {
+	WebDriver driver;
+	By source=By.xpath("//input[@name='ONWARD_ORIGIN_CITY']");
+	By destination =By.xpath("//input[@name='ONWARD_DESTINATION_CITY']");
+	By journeydate=By.xpath("//input[@name='TRIPSTARTDATE']");
+	By search=By.xpath("//input[@value='Search']");
+	By daytotavel=By.xpath("//tbody/tr[1]/td[3]/a[1]");
+	
+	public vrlhomepage(WebDriver driver) {
+		this.driver=driver;
+	}
+	public void sourcedata(String departureplace) {
+		driver.findElement(source).sendKeys(departureplace);
+	}
+	public void destinationdata(String arrivalplace) {
+		driver.findElement(destination).sendKeys(arrivalplace);
+	}
+	public void date() {
+		driver.findElement(journeydate).click();
+		driver.findElement(daytotavel).click();
+	}
+	public void check() {
+		driver.findElement(search).click();
+	}
+
+}
---
+++ vrltravels/src/test/java/vrltravelstest/vrlhometest.java
@@ -0,0 +1,45 @@
+package vrltravelstest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import vrltravelspage.vrlhomepage;
+
+
+
+
+
+public class vrlhometest {
+
+	WebDriver driver;
+	String baseurl="https://www.vrlbus.in/vrl2013/booking_home_page.aspx";
+	
+	@BeforeTest
+	public void setup() {
+		
+		
+		System.setProperty("webdriver.chrome.driver","C:\\\\Users\\\\Nidhin\\\\Downloads\\\\Software Testing\\\\selenium\\\\chrome driver 94\\\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	@BeforeMethod
+	public void start() {
+		driver.get(baseurl);
+	}
+	@Test
+	public void searchh() {
+		
+		vrlhomepage vh=new vrlhomepage(driver);
+		vh.sourcedata("bengaluru");
+		vh.destinationdata("ahamedabad");
+		vh.date();
+		vh.check();
+	}
+	
+}
+
