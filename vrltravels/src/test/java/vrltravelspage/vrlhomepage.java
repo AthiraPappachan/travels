017aa12228f0fd193aa5e1793797a1b472e9abfe
AthiraPappachan <athirapappachan1992@gmail.com>
2021-12-07 11:02:56 +0000

    Home page search

--- vrltravels/src/test/java/vrltravelspage/vrlhomepage.java
+++ vrltravels/src/test/java/vrltravelspage/vrlhomepage.java
@@ -1,7 +1,12 @@
 package vrltravelspage;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class vrlhomepage {
 	WebDriver driver;
@@ -9,24 +14,30 @@ public class vrlhomepage {
 	By destination =By.xpath("//input[@name='ONWARD_DESTINATION_CITY']");
 	By journeydate=By.xpath("//input[@name='TRIPSTARTDATE']");
 	By search=By.xpath("//input[@value='Search']");
-	By daytotavel=By.xpath("//tbody/tr[1]/td[3]/a[1]");
+	
 	
 	public vrlhomepage(WebDriver driver) {
 		this.driver=driver;
 	}
-	public void sourcedata(String departureplace) {
-		driver.findElement(source).sendKeys(departureplace);
-	}
-	public void destinationdata(String arrivalplace) {
-		driver.findElement(destination).sendKeys(arrivalplace);
-	}
-	public void currentdate() {
-		driver.findElement(journeydate).click();
-		driver.findElement(daytotavel).click();
+	public void journey() {
+		driver.findElement(source).sendKeys("bangalore");
+		driver.findElement(destination).sendKeys("indi");
+		driver.findElement(journeydate).clear();
+		driver.findElement(journeydate).sendKeys("09-12-2021");
+		driver.findElement(search).click();
 		
 	}
-	public void check() {
-		driver.findElement(search).click();
+	public void titileverification() {
+		String exptitile="https://www.vrlbus.in/vrl2013/booking_home_page.aspx?q=LWYTJX";
+		String actual=driver.getTitle();
+		assertEquals(actual, exptitile);
 	}
-
+	public void totalLinks() {
+		List<WebElement> alllinks=driver.findElements(By.tagName("a"));
+		System.out.println(alllinks.size());
+		for(WebElement link:alllinks) {
+			System.out.println(link.getText()+"-"+link.getAttribute("href"));
+		}
+		
+	}
 }
